4f541d4ce252c4f0bc75102bbc48bdc746c4f38f
bhamini <bhaminishivram@gmail.com>
2021-03-27 13:05:08 +0530

    initial commit

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+class Counter extends Component 
+{
+    render(){
+
+   return <h1> HEllo world </h1> <button>Increment</button>; 
+   // this statment gives error ,because you cannot have multiple elements as a react element  
+    }
+
+} 
+
+export default Counter; 
