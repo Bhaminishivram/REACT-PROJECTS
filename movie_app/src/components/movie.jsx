f5f7b068fa580af735db8f4c023297e110454c5f
bhamini <bhaminishivram@gmail.com>
2021-03-27 14:51:59 +0530

    update

--- movie_app/src/components/movie.jsx
+++ movie_app/src/components/movie.jsx
@@ -1,9 +1,9 @@
 import  React ,{Component} from 'react' 
- 
+import {getMovies} from '../services/fakeMovieService';
 class Movies extends  Component
 {
     state={
-        count:0
+        movies: getMovies()
      };
     render()
     {
@@ -13,7 +13,30 @@ class Movies extends  Component
               //<h1> {name} </h1> 
              // <h1> {this.state.count}</h1> 
              // <h1>{'my Name is ${name}'}</h1> 
-    <h1>Hello React</h1>
+       //<h1>Hello React</h1> 
+       <table className="table">
+           <thead>
+               <tr>
+                   <th>title</th> 
+                   <th> Genre</th>
+                   <th>Stock</th>
+                   <th>Rate</th>
+               </tr>
+           </thead> 
+           <tbody> 
+               {this.state.movies.map(movie =>(
+                <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td> 
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                
+            </tr>
+                
+               ))}
+               
+           </tbody>
+       </table>
 
 
 
