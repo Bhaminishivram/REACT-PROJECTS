9ed84baef5f150ac4ceaa0512cc0e7557e507d10
bhamini <bhaminishivram@gmail.com>
2021-03-27 14:09:31 +0530

    movieapp

--- movie_app/src/components/movie.jsx
+++ movie_app/src/components/movie.jsx
@@ -2,11 +2,17 @@ import  React ,{Component} from 'react'
  
 class Movies extends  Component
 {
-    state={ }
+    state={
+        count:0
+     };
     render()
     {
         return(
-            <h1> Movies List</h1>
+           // <h1> Movies List</h1> 
+              // <h1> {2*2*6}</h1> //regular experssion in react js  
+              //<h1> {name} </h1> 
+              <h1> {this.state.count}</h1> 
+             // <h1>{'my Name is ${name}'}</h1>
         );
     }
 } 
