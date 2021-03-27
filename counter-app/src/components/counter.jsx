7af5a8259ec34afbc7d35595f09000b3e6ffa070
bhamini <bhaminishivram@gmail.com>
2021-03-27 16:42:53 +0530

    counter_app_update

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,12 +1,41 @@
 import React,{Component} from 'react';
 class Counter extends Component 
-{
+ { 
+     state={ 
+         count:0
+     }; 
+   
+     handleIncrement =() =>
+     {
+         this.setState({
+             count:this.state.count+1
+         }) 
+
+     }; 
+     handleDecrement =()=>
+     {
+         this.setState({
+             count:this.state.count-1
+         }) 
+     }
     render(){
+       
+     return(
+         <div> 
+             <button onClick={this.handleDecrement} className = "btn btn-danger">Decrement</button> 
+             <span style={{fontSize:100}}>{this.formatCount()}</span>
+             <button onClick={this.handleIncrement} className = "btn btn-primary">Increment</button> 
+         </div>
+     );
 
-   return <h1> HEllo world </h1> <button>Increment</button>; 
-   // this statment gives error ,because you cannot have multiple elements as a react element  
-    }
+       } 
 
-} 
+       formatCount(){
+           const{ count } = this.state; 
+           return count === 0 ? 'Zero' : count;
+       } 
+
+}  
+ 
 
 export default Counter; 
