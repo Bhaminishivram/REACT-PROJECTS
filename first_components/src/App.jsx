9eb1e5aa57f743190bf4392a65c1113b9e0e19f6
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-14 13:19:50 +0530

    first_jsxprogram commit

---
+++ first_components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+class App extends React.Component{
+  render(){
+    return(
+      <div> 
+      <h1>Hello world;</h1>
+      <h2>Header</h2>
+      <h3>Content</h3> 
+      <p>This is the content</p>
+      </div>
+    )
+  }
+}
+export default App;
---
+++ login_page/src/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import From  from './Form'; 
+import './Contactstyle.css'; 
+
+const Contact = () => {
+    return (
+        <div className = "contactstyle">
+            <h2> Welcome to contact page </h2>
+        </div>
+    )
+} 
+export default Contact;
