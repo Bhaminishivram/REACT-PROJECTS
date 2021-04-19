f858bd747de24294c914ce93d41d39728f4efd33
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-19 11:46:52 +0530

    first_components  commit

--- first_components/src/App.jsx
+++ first_components/src/App.jsx
@@ -6,7 +6,9 @@ class App extends React.Component{
       <h1>Hello world;</h1>
       <h2>Header</h2>
       <h3>Content</h3> 
-      <p>This is the content</p>
+      <p>This is the content</p> 
+      <h1>{this.props.headerProp}</h1>
+      <h2>{this.props.contentProp}</h2>
       </div>
     )
   }
