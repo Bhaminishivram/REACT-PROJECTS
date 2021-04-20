316bb1b76e6628c3fef61275ee7ac7b2748ac6de
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-20 13:46:18 +0530

    like_functional

--- movie_app/src/components/like.jsx
+++ movie_app/src/components/like.jsx
@@ -1,12 +1,32 @@
 import React,{Component} from 'react'; 
+ 
+  // functional components 
+const Like = (props) => { 
+    let classes = "fa fa-heart"; 
+    if(!props.likeStatus) classes+= '-o';
+    return ( 
+        <i className={classes} onClick={props.onLikebutton} style ={{cursor:'pointer'}}></i>
+     );
+}
+ 
+export default Like;
 
-class Like extends Component{
-    render(){ 
-        let classes = "fa fa-heart"; 
-        if(!this.props.likeStatus) classes+='-o' 
-        return (
-            <i className={classes} onClick={this.props.onLikebutton}></i>
-        );
-    } 
-} 
-export default Like; 
+
+
+
+
+     // class component 
+// class Like extends Component{
+//     render(){ 
+//         let classes = "fa fa-heart"; 
+//         if(!this.props.likeStatus) classes+='-o' 
+//         return (
+//             <i className={classes} onClick={this.props.onLikebutton} style ={{cursor:'pointer'}}></i>
+//         );
+//     } 
+// } 
+// export default Like;  
+
+
+
+// note : this use only for class component
