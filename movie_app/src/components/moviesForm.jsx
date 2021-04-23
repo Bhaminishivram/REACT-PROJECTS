815c989313ba9000bf8ed2cbf62a31c0788c12cd
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-23 18:09:59 +0530

    movies_form_id save_part2

--- movie_app/src/components/moviesForm.jsx
+++ movie_app/src/components/moviesForm.jsx
@@ -1,7 +1,10 @@
 import React from 'react'; 
-const MovieForm = ({match}) => {
-    return (  
-        <h1> Movies From {match.params.id}</h1>
+const MovieForm = ({match,history}) => {
+    return ( 
+        <div>
+        <h1> Movies From {match.params.id}</h1> 
+        <button className="btn btn-primary" onClick={()=>{history.push('/movies')}}>save</button>
+        </div>
     );
 }
 
