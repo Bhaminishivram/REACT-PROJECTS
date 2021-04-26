061e6250f95bf3cc8a7ba8cd43f7cca0f8988df7
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-04-26 09:52:34 +0530

    how to add login form in navbar

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -7,20 +7,22 @@ import Navbar from './components/navbar';
 import Customer from './components/customer'; 
 import Rentals from './components/rentals'; 
 import NotFound from './components/notfound'; 
-import moviesForm from './components/moviesForm';
+import moviesForm from './components/moviesForm'; 
+import Loginform from './components/loginform';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar /> 
       <main className="container">
-        <Switch>  
+        <Switch>   
           <Route path="/movies/:id" component={moviesForm}/>
           <Route path="/movies" component={Movies}/> 
           <Route path="/customer" component={Customer} /> 
           <Route path="/rentals" component={Rentals}/>  
+          <Route path="/login" component={Loginform}/>  
           <Route path="/notfound" component={NotFound}/>
-          <Route path="/" exact component={Movies}/> 
+          <Route path="/" exact component={Movies}/>  
           <Redirect to="/notfound"/>
         </Switch>
       </main>
